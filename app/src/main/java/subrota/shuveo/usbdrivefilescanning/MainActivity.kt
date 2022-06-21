0e2f792074b24d964f4194a55f308dab37fdb4d1
Subrota Debnath <subrotadebnath1807@gmail.com>
2022-06-21 18:08:32 +0600

    usb scanner environment

--- app/src/main/java/subrota/shuveo/usbdrivefilescanning/MainActivity.kt
+++ app/src/main/java/subrota/shuveo/usbdrivefilescanning/MainActivity.kt
@@ -3,6 +3,7 @@ package subrota.shuveo.usbdrivefilescanning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import me.jahnen.libaums.core.UsbMassStorageDevice
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val devices = UsbMassStorageDevice.getMassStorageDevices(this /* Context or Activity */)
+        val fromUsbButton = findViewById<Button>(R.id.usbBTN)
 
-        for (device in devices) {
 
-            // before interacting with a device you need to call init()!
-            device.init()
 
-            // Only uses the first partition on the device
-            val currentFs = device.partitions[0].fileSystem
-            Log.i(tag, "Capacity: " + currentFs.capacity)
-            Log.i(tag, "Occupied Space: " + currentFs.occupiedSpace)
-            Log.i(tag, "Free Space: " + currentFs.freeSpace)
-            Log.i(tag, "Chunk size: " + currentFs.chunkSize)
+
+        fromUsbButton.setOnClickListener {
+            Log.i(tag, "Usb button clicked")
+
+
+            val devices = UsbMassStorageDevice.getMassStorageDevices(this /* Context or Activity */)
+
+            for (device in devices) {
+
+                // before interacting with a device you need to call init()!
+                device.init()
+
+                // Only uses the first partition on the device
+                val currentFs = device.partitions[0].fileSystem
+                Log.i(tag, "Capacity: " + currentFs.capacity)
+                Log.i(tag, "Occupied Space: " + currentFs.occupiedSpace)
+                Log.i(tag, "Free Space: " + currentFs.freeSpace)
+                Log.i(tag, "Chunk size: " + currentFs.chunkSize)
+            }
         }
     }
 }
